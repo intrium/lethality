f5fe29238b90239861640f0263c66c9fce9b36c9
ian <87878603+huysuh@users.noreply.github.com>
2025-02-24 23:01:19 -0500

    Delete inject.lua

--- inject.lua
+++
@@ -1,9 +0,0 @@
-
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/ui.lua"))()
-
--- modules
-local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/esp.lua"))()
-
-local destroyESP = ESPModule:Initialize(GUI)
-
--- destroyESP()
