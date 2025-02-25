2d17fd3c7ba44621b6ab571b3273789e12c4d805
ian <87878603+huysuh@users.noreply.github.com>
2025-02-24 23:41:11 -0500

    Update inject.lua

--- inject.lua
+++ inject.lua
@@ -1,9 +1,14 @@
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/ui.lua"))()
+local GUI = loadstring(game:HttpGet("https://pastebin.com/raw/UzWgkFfK"))()
+GUI:Initialize()
 
 -- modules
-local ESPModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/esp.lua"))()
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/esp.lua"))()
+local Trigger = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/trigger.lua"))()
+local Watermark  = loadstring(game:HttpGet("https://raw.githubusercontent.com/intrium/lethality/refs/heads/main/watermark.lua"))()
 
-local destroyESP = ESPModule:Initialize(GUI)
+local destroyESP = ESP:Initialize(GUI)
+local destroyTrigger = Trigger:Initialize(GUI)
+local destroyWatermark = Watermark:Initialize(GUI)
 
 -- destroyESP()
